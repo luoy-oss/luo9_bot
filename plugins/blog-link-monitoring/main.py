a90eec9825a1c106ca4981d1518dc01c9243cb3a
drluo <2557657882@qq.com>
2025-05-23 22:12:19 +0800

    为blog-link-monitoring插件添加额外url列表 允许不使用friend_link_url

--- plugins/blog-link-monitoring/main.py
+++ plugins/blog-link-monitoring/main.py
@@ -43,35 +43,42 @@ if not __config['minutes'] or __config['minutes'] < 0:
 
 @driver.on_startup
 async def _():
-    log.info("blog-link-monitoring>>>>start")
+    log.info("blog-link-monitoring >>>> start")
 
 # 每5分钟进行一次请求
-@task.on_schedule_task(trigger ='interval', minutes=__config['minutes'])
+@task.on_schedule_task(trigger ='interval', minutes=__config['minutes'], misfire_grace_time=16)
 async def _api_task():
     await monitoring_api()
 
 async def monitoring_api():
     api_url = f"{__config['api_url']}/api/batch-monitor"
-    friend_link_url = __config['friend_link_url']
-
-    params = {}
-    response = requests.get(friend_link_url, params=params)
-
-    url_list = []
-    if response.status_code == 200:
-        response_json = response.json()
-        log.info("github友链获取成功")
-        
-        urls = response_json
-        for item in urls:
-            issue_body = item['body']
-            pattern = '```json\n([\s\S]*?)\n```'
-            matchObj = re.search(pattern, issue_body)
-            if matchObj and matchObj[1]:
-                link_data = json.loads(matchObj[1])
-                url_list.append(link_data['url'])
+    if __config.get('friend_link_url'):
+        friend_link_url = __config['friend_link_url']
     else:
-        log.warning(response.status_code, response.text)
+        friend_link_url = ''
+
+    if __config.get('lists'):
+        url_list = __config['lists']
+    else:
+        url_list = []
+
+    if friend_link_url:
+        params = {}
+        response = requests.get(friend_link_url, params=params)
+        if response.status_code == 200:
+            response_json = response.json()
+            log.info("github友链获取成功")
+            
+            urls = response_json
+            for item in urls:
+                issue_body = item['body']
+                pattern = '```json\n([\s\S]*?)\n```'
+                matchObj = re.search(pattern, issue_body)
+                if matchObj and matchObj[1]:
+                    link_data = json.loads(matchObj[1])
+                    url_list.append(link_data['url'])
+        else:
+            log.warning(response.status_code, response.text)
 
     data = {
         'urls': url_list
